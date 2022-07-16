0ef68f0c3cd2ff90cd3c3e608e8dadf15f76618c
Vitaliy Teleusov <Vitaliy82tvn@gmail.com>
2022-07-16 15:24:28 +0300

    Add changes

--- Sources/commonUtils/Extensions/UIDeviceExtensions.swift
+++ Sources/commonUtils/Extensions/UIDeviceExtensions.swift
@@ -10,21 +10,40 @@ import LocalAuthentication
 
 public extension UIDevice {
     
-    static func getBiometricsType() -> LABiometryType {
-        let context = LAContext()
-        var error: NSError?
-        guard context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
-            return .none
+    func isFaceIdSupported() -> Bool {
+        if #available(iOS 11.0, *) {
+            let context = LAContext()
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) {
+                return context.biometryType == LABiometryType.faceID
+            }
         }
-        return context.biometryType
+
+        return false
     }
     
-    static func isThereFaceId() -> Bool {
-        switch getBiometricsType() {
-        case .faceID:
-            return true
-        default:
-            return false
+    func isTouchIdSupported() -> Bool {
+        if #available(iOS 11.0, *) {
+            let context = LAContext()
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) {
+                return context.biometryType == LABiometryType.touchID
+            }
+        }
+
+        return false
+    }
+    
+//    var hasNotch: Bool {
+//        let bottom = UIApplication.shared.keyWindow?.safeAreaInsets.bottom ?? 0
+//        return bottom > 0
+//    }
+    
+    var hasNotch: Bool {
+        guard #available(iOS 11.0, *), let window = UIApplication.shared.keyWindow else { return false }
+        let orientation = UIApplication.shared.statusBarOrientation
+        if orientation.isPortrait {
+            return window.safeAreaInsets.top >= 44
+        } else {
+            return window.safeAreaInsets.left > 0 || window.safeAreaInsets.right > 0
         }
     }
 }
